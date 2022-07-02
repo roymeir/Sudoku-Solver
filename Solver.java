71c724008e6433f1e12f7b37bb197a21605b7b7f
roymeir <roy.meir@post.idc.ac.il>
2022-07-02 18:27:45 +0300

    Sudoku Generator
    Added function to let user input cell values into initial board for the computer to solve.

--- Solver.java
+++ Solver.java
@@ -42,11 +42,40 @@ public class Solver
         return true;
     }
 		
-
+    public static int[][] sudokuGenerator(int N){
+        /*gives user option to create customized sudoko board. Uses the isValid function 
+        to make sure user input is valid before progressing to the next cell. The only input is the size of the board.
+        If the user inputs 0, that means the given cell is to be left empty.*/
+        int[][] sudoku_board = new int [N][N];
+        for (int i=0;i<N;i++){
+            for (int j=0; j<N; j++){
+                System.out.println("Enter value of cell "+(i+1)+" "+(j+1));
+                Scanner scan = new Scanner(System.in);
+                int attempt = scan.nextInt();
+                if (attempt == 0){
+                    sudoku_board[i][j] = attempt;
+                }
+                else{
+                    while (!isValid(sudoku_board, i, j, attempt)){
+                        System.out.println("This entry is invalid. Try another number: ");
+                        attempt = scan.nextInt();
+                    }
+                }
+                sudoku_board[i][j] = attempt;    
+            }
+        }
+        return sudoku_board;
+    }
 	// Driver Code
 	public static void main(String args[])
 	{
-       
+       int [][] board = sudokuGenerator(3);
+       for (int i=0;i<3;i++){   //making sure the board was created successfully 
+        for (int j=0;j<3;j++){
+            System.out.print(board[i][j]+" ");
+        }
+        System.out.println();
+       }
 	}
 }
 
