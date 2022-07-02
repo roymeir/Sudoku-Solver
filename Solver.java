5d60e1442b78d32c8b02b8cb9ee07d31e98dadf4
roymeir <roy.meir@post.idc.ac.il>
2022-07-02 19:32:34 +0300

    Printing Function
    Added an external printing function

--- Solver.java
+++ Solver.java
@@ -50,18 +50,19 @@ public class Solver
         for (int i=0;i<N;i++){
             for (int j=0; j<N; j++){
                 System.out.println("Enter value of cell "+(i+1)+" "+(j+1));
-                Scanner scan = new Scanner(System.in);
-                int attempt = scan.nextInt();
-                if (attempt == 0){
-                    sudoku_board[i][j] = attempt;
-                }
-                else{
-                    while (!isValid(sudoku_board, i, j, attempt)){
-                        System.out.println("This entry is invalid. Try another number: ");
-                        attempt = scan.nextInt();
+                try (Scanner scan = new Scanner(System.in)) {
+                    int attempt = scan.nextInt();
+                    if (attempt == 0){
+                        sudoku_board[i][j] = attempt;
                     }
-                }
-                sudoku_board[i][j] = attempt;    
+                    else{
+                        while (!isValid(sudoku_board, i, j, attempt)){
+                            System.out.println("This entry is invalid. Try another number: ");
+                            attempt = scan.nextInt();
+                        }
+                    }
+                    sudoku_board[i][j] = attempt;
+                }    
             }
         }
         return sudoku_board;
@@ -73,6 +74,7 @@ public class Solver
          */
         int row = -1;
 		int col = -1;
+        /* Initialized to -1 so as to not point to a specific row or column */
 		boolean isEmpty = true;
 		for (int i = 0; i < N; i++)
 		{
@@ -87,11 +89,15 @@ public class Solver
 					break;
 				}
 			}
-			if (!isEmpty) { /* Break out of the external loop and not only the internal one */
+
+			if (!isEmpty) 
+            { /* Break out of the external loop and not only the internal one */
 				break;
 			}
 		}
-		// No empty cells left
+		/* If we have no more empty cells, return true.
+         * This will serve as the ending point of all recursive calls. 
+         */
 		if (isEmpty)
 		{
 			return true;
@@ -104,46 +110,58 @@ public class Solver
 				puzzle[row][col] = number;
 				if (solve_sudoku(puzzle, N))
 				{
-					// checks if the addition we made makes the board valid 
+					/* This will return true if the value we entered is correct. It may have been correct on entry,
+                    but it could create an error once all empty cells are filled. */ 
 					return true;
 				}
 				else
 				{
-					// replace it
+					/* The addition was valid at the time of insertion, but once other cells were filled we found a problem.
+                     * This means the value we put in the cell was not the right one and we empty the cell to try again.
+                     */
 					puzzle[row][col] = 0;
 				}
 			}
 		}
 		return false;   //If we've reached this point, there is not solution available for the given board
     }
-
+    public static void print_puzzle(int [][]puzzle, int N){
+        System.out.println("Board after solution:");
+        for (int i=0;i<9;i++)
+        {    
+            for (int j=0;j<9;j++)
+            {
+                System.out.print(puzzle[i][j]+" ");
+            }
+            System.out.println();  
+        }       
+    }
     // Driver Code
 	public static void main(String args[])
 	{
-        int[][] board = new int[][] {       
-            { 1, 5, 0, 0, 4, 2, 0, 0, 6 },
-			{ 2, 7, 4, 5, 6, 0, 0, 1, 0 },
-			{ 0, 0, 6, 0, 0, 7, 4, 0, 2 },
-			{ 0, 1, 0, 0, 0, 0, 0, 4, 0 },
-			{ 0, 0, 0, 0, 5, 0, 0, 0, 0 },
-			{ 0, 6, 0, 4, 0, 3, 1, 9, 0 },
-			{ 0, 2, 0, 6, 0, 5, 9, 0, 0 },
-			{ 9, 8, 5, 0, 3, 0, 0, 6, 0 },
-			{ 0, 4, 0, 2, 1, 9, 8, 3, 0 }
+        int[][] puzzle = new int[][] {
+			{ 3, 0, 5, 4, 0, 2, 0, 6, 0 },
+			{ 4, 9, 0, 7, 6, 0, 1, 0, 8 },
+			{ 6, 0, 0, 1, 0, 3, 2, 4, 5 },
+			{ 0, 0, 3, 9, 0, 0, 5, 8, 0 },
+			{ 9, 6, 0, 0, 5, 8, 7, 0, 3 },
+			{ 0, 8, 1, 3, 0, 4, 0, 9, 2 },
+			{ 0, 5, 0, 6, 0, 1, 4, 0, 0 },
+			{ 2, 0, 0, 5, 4, 9, 0, 7, 0 },
+			{ 1, 4, 9, 0, 0, 7, 3, 0, 6 }
 		};
-       if (solve_sudoku(board, 9)){
-        for (int i=0;i<9;i++){   //making sure the board was created successfully 
-            for (int j=0;j<9;j++){
-                System.out.print(board[i][j]+" ");
-            }
-            System.out.println();
+        System.out.println("Input board before solution:");
+        print_puzzle(puzzle, puzzle.length);
+        System.out.println("");
+
+        if (solve_sudoku(puzzle, puzzle.length))    //Making sure the board was created successfully
+        {
+            print_puzzle(puzzle, puzzle.length);
+        }
+        else
+           {
+            System.out.println("This can't be solved");
            }
-        
-       }
-       else{
-        System.out.println("This can't be solved");
-       }
-       
 	}
 }
 
