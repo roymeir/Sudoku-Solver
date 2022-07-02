43552a7ea727f938fb09465c97e31e854256e8f4
roymeir <roy.meir@post.idc.ac.il>
2022-07-02 19:10:05 +0300

    Solver Function added.
    Added the function that actually solves the puzzle. it uses recursion to check its entries until every cell has the correct number in it.

--- Solver.java
+++ Solver.java
@@ -66,16 +66,84 @@ public class Solver
         }
         return sudoku_board;
     }
-	// Driver Code
+	
+    public static boolean solve_sudoku(int[][]puzzle, int N){
+        /* This is the actual code that solves the puzzle.
+         * If no solution exists, the function will return false.
+         */
+        int row = -1;
+		int col = -1;
+		boolean isEmpty = true;
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				if (puzzle[i][j] == 0)
+				{
+                    /* First instance of an empty cell, represented by a 0 */
+					row = i;
+					col = j;
+					isEmpty = false;
+					break;
+				}
+			}
+			if (!isEmpty) { /* Break out of the external loop and not only the internal one */
+				break;
+			}
+		}
+		// No empty cells left
+		if (isEmpty)
+		{
+			return true;
+		}
+		/* If we do have empty cells, we use backtracking to enter the correct digit in every empty cell */ 
+		for (int number = 1; number <= N; number++)
+		{
+			if (isValid(puzzle, row, col, number))
+			{
+				puzzle[row][col] = number;
+				if (solve_sudoku(puzzle, N))
+				{
+					// checks if the addition we made makes the board valid 
+					return true;
+				}
+				else
+				{
+					// replace it
+					puzzle[row][col] = 0;
+				}
+			}
+		}
+		return false;   //If we've reached this point, there is not solution available for the given board
+    }
+
+    // Driver Code
 	public static void main(String args[])
 	{
-       int [][] board = sudokuGenerator(3);
-       for (int i=0;i<3;i++){   //making sure the board was created successfully 
-        for (int j=0;j<3;j++){
-            System.out.print(board[i][j]+" ");
-        }
-        System.out.println();
+        int[][] board = new int[][] {       
+            { 1, 5, 0, 0, 4, 2, 0, 0, 6 },
+			{ 2, 7, 4, 5, 6, 0, 0, 1, 0 },
+			{ 0, 0, 6, 0, 0, 7, 4, 0, 2 },
+			{ 0, 1, 0, 0, 0, 0, 0, 4, 0 },
+			{ 0, 0, 0, 0, 5, 0, 0, 0, 0 },
+			{ 0, 6, 0, 4, 0, 3, 1, 9, 0 },
+			{ 0, 2, 0, 6, 0, 5, 9, 0, 0 },
+			{ 9, 8, 5, 0, 3, 0, 0, 6, 0 },
+			{ 0, 4, 0, 2, 1, 9, 8, 3, 0 }
+		};
+       if (solve_sudoku(board, 9)){
+        for (int i=0;i<9;i++){   //making sure the board was created successfully 
+            for (int j=0;j<9;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+           }
+        
        }
+       else{
+        System.out.println("This can't be solved");
+       }
+       
 	}
 }
 
